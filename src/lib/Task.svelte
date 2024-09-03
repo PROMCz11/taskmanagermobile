74491432e8c87b4738eec7ab33e212463f5e89a4
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-09-03 14:54:10 +0300

    First test

---
+++ src/lib/Task.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let important = false;
+    export let completed = false;
+
+    import uncheckedRadioSrc from "$lib/assets/task-radio-unchecked-icon.svg"
+    import checkedRadioSrc from "$lib/assets/task-radio-checked-icon.svg"
+</script>
+
+<div class="task" class:important class:completed>
+    <div>
+        <p class="content" contenteditable>This would be the task's content</p>
+        <p class="date">Sun 12/4 10:34 AM</p>
+    </div>
+    {#if completed}
+        <button on:click={() => completed = !completed} class="completed-toggle"><img src={checkedRadioSrc} alt="complete"></button>
+    {:else}
+        <button on:click={() => completed = !completed} class="completed-toggle"><img src={uncheckedRadioSrc} alt="complete"></button>
+    {/if}
+</div>
+
+<style>
+    .task {
+        border: 2px solid transparent;
+        border-radius: .5rem;
+        background-color: #222222;
+        padding: .5rem;
+        display: flex;
+        justify-content: space-between;
+        gap: .5rem;
+        align-items: center;
+    }
+
+    .date {
+        color: #757575;
+    }
+
+    .task.important {
+        border-color: red;
+    }
+
+    .task.completed {
+        border-color: green;
+    }
+
+    .task.completed .content {
+        text-decoration: line-through;
+    }
+
+    .completed-toggle {
+        background-color: transparent;
+        border: none;
+        padding: 0;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Task from "$lib/Task.svelte";
+</script>
+
+<div>
+    <Task />
+    <Task />
+    <Task important={true}/>
+    <Task />
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+        padding: .5rem;
+    }
+</style>
+
+<div style="padding: 1rem;" class="to-be-deleted">
+    <p>note: upon clicking a task, a swipe up panel appears from the bottom of the screen, containing details such as important and completed status, id, last updated, and the delete button</p>
+    
+    <p>v 1.0.0</p>
+</div>
