a422d1f361e7819b2f51be73ca78e07c3aecd86a
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-09-15 22:40:32 +0300

    Removing unnecessary v in front of the version name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -174,7 +174,7 @@
                     Loading...
                 {/if}
             </p>
-            <p class="version">v 1.10.23</p>
+            <p class="version">1.10.23</p>
             <div class="filters">
                 <button on:click={() => filterCode = 0} class="all-filter" class:active={filterCode === 0}>
                     <img src={allTasksIconSrc} alt="all tasks">
